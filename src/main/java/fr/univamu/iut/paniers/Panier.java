104cfa1824592006050c8350d1f27aff1ecd11e5
doop <flavio.oderzo@etu.univ-amu.fr>
2025-03-25 15:18:16 +0100

    feat: setup paniers

---
+++ src/main/java/fr/univamu/iut/paniers/Panier.java
@@ -0,0 +1,15 @@
+package fr.univamu.iut.paniers;
+
+import java.util.Date;
+
+/**
+ * Classe représentant un panier
+ */
+public class Panier {
+    private int id_panier;
+    private float prix;
+    private int qtt_panier_dispo;
+    private Date derniere_maj;
+
+
+}
